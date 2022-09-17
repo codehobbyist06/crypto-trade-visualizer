4cd40b4253ea16b7f6cfa13e4c2bcbbf1407a7bf
Anshul Pandey <65885804+codehobbyist06@users.noreply.github.com>
2022-09-18 00:40:26 +0530

    Point proxy url to AWS deployed API

--- script.js
+++ script.js
@@ -14,7 +14,8 @@ let other_user_data = [];
 let is_valid_name = {};
 
 const api_url = 'https://api.paxful.com/';
-const cors_proxy_url = 'https://trade-visualizer-backend.herokuapp.com';
+// const cors_proxy_url = 'https://trade-visualizer-backend.herokuapp.com';
+const cors_proxy_url = 'http://3.111.169.99:5000/';
 const cors_dev_url = 'http://127.0.0.1:5000';
 
 // Fetch all the user data mentioned in the valid usernames list
